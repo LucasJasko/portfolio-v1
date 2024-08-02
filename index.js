43f091576884dfcd93490dfb759c34744e3d6f05
unknown <lucasjaskowiak@yahoo.fr>
2024-08-02 02:34:59 +0200

    Implémentation de la timeline dynamique

--- index.js
+++ index.js
@@ -31,3 +31,36 @@ window.addEventListener("mousemove", (e) => {
 window.addEventListener("scroll", () => {
     mouse.style.position = "fixed";
 });
+
+
+let ScrollTrigger = true;
+
+window.addEventListener("scroll", () => {
+    const BlacklineArea = document.querySelector(".blackline-area")
+    scrollLvl = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    scrollTop = window.scrollY / document.body.offsetHeight;
+    if (scrollTop > 0.25) {
+        BlacklineArea.style.position = "fixed";
+        BlacklineArea.style.width = "25%";
+        BlacklineArea.style.top = "0";
+        BlacklineArea.style.left = "0";
+        if (ScrollTrigger) {
+            ScrollTrigger = false;
+            const PositionDot = document.createElement("span");
+            PositionDot.classList.add("position-dot");
+            BlacklineArea.appendChild(PositionDot);}
+        }
+        const PositionDot = document.querySelector(".position-dot");
+        if (PositionDot) {
+        PositionDot.style.top = (scrollTop - 0.1) * 100 + "%";
+    }
+        if (scrollTop < 0.25) {
+            BlacklineArea.style.position = "static";
+            BlacklineArea.style.width = "100%";
+            ScrollTrigger = true;
+            const PositionDot = document.querySelector(".position-dot");
+            if (PositionDot) {
+            PositionDot.remove();
+        }
+    }
+});
