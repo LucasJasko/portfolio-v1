8979c5b30b68aef00e7f93b403d60afa64c2ff82
LucasJasko <lucasjaskowiak@yahoo.fr>
2025-09-15 17:07:44 +0200

    style: refonte visuel intégrale du site

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MonCV
+
 Ce projet est réalisé dans l'objectif de montrer mes expériences à travers mes compétences
+
+TODO remanier version mobile
+TODO introduire switch version française / anglaise
+TODO intrduire switch version clair / sombre
--- index.js
+++ index.js
@@ -1,109 +1,297 @@
-// Paramétrage des beams du background
-// const beamMaker = () => {
-//   const beam = document.createElement("span");
-//   beam.classList.add("beam");
-//   document.getElementById("beam-display").appendChild(beam);
-
-//   beam.style.setProperty("--beamt", Math.random() * 100 + "%");
-//   beam.style.setProperty("--beaml", Math.random() * 100 + "%");
-
-//   setTimeout(() => {
-//     beam.remove();
-//   }, 6000);
-// };
-// setInterval(beamMaker, 3000);
-
-// Lettrage coloré du titre
-let letters = document.querySelector(".txt").textContent;
-const letterArray = letters.split("");
-document.querySelector(".txt").textContent = "";
-letterArray.forEach((element, i) => {
-  span = "<span>" + element + "</span>";
-  document.querySelector(".txt").innerHTML += span;
-});
-
-// Configuration du point rouge de localisation et du fix de la timeline
-const blacklineArea = document.querySelector(".blackline-area");
-const timeline = document.getElementById("timeline-area");
-let ScrollTrigger = true;
-window.addEventListener("scroll", () => {
-  scrollTop = window.scrollY / document.body.offsetHeight;
-  scrollLvl = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-
-  if (timeline.getBoundingClientRect().top <= 0) {
-    blacklineArea.style.position = "fixed";
-    blacklineArea.style.width = "255px";
-    blacklineArea.style.top = "0";
-    if (ScrollTrigger) {
-      ScrollTrigger = false;
-      const PositionDot = document.createElement("span");
-      PositionDot.classList.add("position-dot");
-      blacklineArea.appendChild(PositionDot);
-    }
-  }
-  const PositionDot = document.querySelector(".position-dot");
-  if (PositionDot) {
-    PositionDot.style.top = (window.scrollY / timeline.clientHeight - 0.25) * 100 + "%";
-  }
-  if (timeline.getBoundingClientRect().top > 0) {
-    blacklineArea.style.position = "static";
-    blacklineArea.style.width = "100%";
-    ScrollTrigger = true;
-    const PositionDot = document.querySelector(".position-dot");
-    if (PositionDot) {
-      PositionDot.remove();
-    }
-  }
-  if (timeline.getBoundingClientRect().bottom <= window.innerHeight) {
-    blacklineArea.style.position = "absolute";
-    blacklineArea.style.width = "255px";
-    blacklineArea.style.top = "auto";
-    blacklineArea.style.bottom = "0";
-  }
-});
-
-// comportements de la navbar
-const navBar = document.querySelector("nav");
-const navItems = navBar.querySelectorAll("nav ul li a");
+// NavBar
+const navBar = document.querySelector(".navbar");
+const navItems = navBar.querySelectorAll(".navbar-item");
 const navBgs = navBar.querySelectorAll(".bg-hover");
 const burgerButton = document.querySelector(".burger-button");
+let isNavOpen = true;
 
-lastPos = 0;
+// Timeline
+const blackline = document.querySelector(".blackline");
+const positionDot = document.querySelector(".position-dot");
+const timelineArea = document.getElementById("timeline-area");
+const timelineContainer = document.querySelector(".timeline-container");
+const timelines = document.querySelectorAll(".timeline");
+const previousArea = document.querySelector(".previous-area");
+const nextArea = document.querySelector(".next-area");
+const cardContainers = document.querySelectorAll(".card-container");
+let initialPos = blackline.getBoundingClientRect().left + (blackline.clientWidth * 2) / 100 - positionDot.clientWidth;
+let timelineIndex = 1;
+let dotPosition;
+let endPos;
+let deltaDotTimelines;
+let timelineToDisplay = 0;
 
-window.addEventListener("scroll", () => {
-  if (window.innerWidth > 730) {
-    if (lastPos < window.scrollY) {
-      navBar.style.transform = "translateY(-100%)";
+cardContainers.forEach((cardContainer) => {
+  const containerTitles = cardContainer.querySelectorAll(".card-title");
+  const containerTexts = cardContainer.querySelectorAll(".card-text");
+
+  containerTitles[0].classList.add("card-title-active");
+
+  containerTitles.forEach((title, i) => {
+    if (i != 0) {
+      return;
     } else {
-      navBar.style.transform = "translateY(0px)";
+      title.classList.add("card-title-active");
     }
-  }
-  lastPos = window.scrollY;
+  });
+
+  containerTexts.forEach((text, i) => {
+    if (i != 0) {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+  });
+
+  containerTitles.forEach((title, titleIndex) => {
+    title.addEventListener("click", () => {
+      containerTitles.forEach((text, i) => {
+        if (i != titleIndex) {
+          text.classList.remove("card-title-active");
+        } else {
+          text.classList.add("card-title-active");
+        }
+      });
+      containerTexts.forEach((text, textIndex) => {
+        if (textIndex != titleIndex) {
+          text.style.display = "none";
+        } else {
+          text.style.display = "block";
+        }
+      });
+    });
+  });
 });
+
+// Strengths
+const titles = document.querySelectorAll(".strengths-headband h4");
+let PrevText = document.getElementById("strength-1");
+let activeTitle = titles[0];
+
+// Projects
+const tagList = document.querySelectorAll(".project-tag");
+const projectList = document.querySelectorAll(".project-card");
+let activeTagList = [];
+
+// Contact
+const textArea = document.querySelectorAll(".text-area");
+const cordP = document.querySelectorAll(".text-area p");
+const contactForm = document.querySelector("#contact-form");
+const errorMessage = document.querySelector(".error-message");
+
+// Top button
+const btnScrollTop = document.getElementById("btnScrollTop");
+let isButtonActive = false;
+let isToDisplay = false;
+
+// comportements de la navbar
 burgerButton.addEventListener("click", () => {
   if (window.innerWidth <= 730) {
-    if (navBar.style.transform == "translateX(-100%)") {
-      navBar.style.transform = "translateX(0px)";
+    if (isNavOpen) {
+      navBar.style.transform = "translateX(-100%) translateY(0)";
+      isNavOpen = false;
     } else {
-      navBar.style.transform = "translateX(-100%)";
+      navBar.style.transform = "translateX(0) translateY(0)";
+      isNavOpen = true;
     }
   }
 });
 navItems.forEach((item) => {
   item.addEventListener("click", () => {
-    if (window.innerWidth <= 730 && navBar.style.transform == "translateX(0px)") {
-      console.log("hello");
-      navBar.style.transform = "translateX(-100%)";
+    if (window.innerWidth <= 730) {
+      navBar.style.transform = "translateX(-100%) translateY(0)";
+      isNavOpen = false;
     }
   });
 });
 
+// Bouton de retour en haut de page
+window.onscroll = function () {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    btnScrollTop.style.display = "flex";
+  } else {
+    btnScrollTop.style.display = "none";
+  }
+};
+btnScrollTop.onclick = function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+// Drag du point de position timeline
+document.addEventListener("dragover", (event) => {
+  event.preventDefault();
+});
+window.addEventListener("resize", () => {
+  initialPos = blackline.getBoundingClientRect().left + (blackline.clientWidth * 3) / 100 - positionDot.clientWidth;
+});
+positionDot.addEventListener("drag", (e) => {
+  e.preventDefault();
+  positionDot.style.transition = "none";
+  endPos = blackline.clientWidth - (blackline.clientWidth * 3) / 100;
+
+  if (e.screenX != 0) {
+    dotPosition = e.clientX - initialPos;
+
+    positionDot.style.left = `${dotPosition}px`;
+
+    if (dotPosition + initialPos - positionDot.clientWidth < initialPos) {
+      dotPosition = 0;
+      positionDot.style.left = `${(blackline.clientWidth * 3) / 100}px`;
+    }
+
+    if (dotPosition + positionDot.clientWidth > blackline.clientWidth) {
+      dotPosition = endPos;
+      positionDot.style.left = `${endPos}px`;
+    }
+
+    deltaDotTimelines = (dotPosition / blackline.clientWidth) * timelineContainer.clientWidth * timelines.length;
+  }
+});
+positionDot.addEventListener("dragend", () => {
+  positionDot.style.transition = "0.2s ease";
+
+  while (deltaDotTimelines > 0) {
+    deltaDotTimelines = deltaDotTimelines - timelineContainer.clientWidth;
+    timelineToDisplay++;
+  }
+
+  timelineContainer.scrollTo(offsetWidth(timelineToDisplay), 0);
+  timelineIndex = timelineToDisplay;
+  if (timelineIndex == 0) {
+    timelineIndex = 1;
+  }
+
+  refreshTimelinesNavButtons(timelineIndex);
+  positionDot.style.left = `${
+    ((blackline.clientWidth - 2 * ((blackline.clientWidth * 3) / 100)) / timelines.length) * timelineToDisplay + (blackline.clientWidth * 3) / 100
+  }px`;
+
+  timelineToDisplay = 0;
+});
+
+// Gestion des zones suivant et retour de la timeline
+refreshTimelinesNavButtons(timelineIndex);
+
+previousArea.addEventListener("mouseenter", () => {
+  previousArea.style.opacity = "1";
+});
+previousArea.addEventListener("mouseleave", () => {
+  previousArea.style.opacity = "0";
+});
+previousArea.addEventListener("click", () => {
+  if (timelineIndex > 0) {
+    timelineIndex--;
+    timelineContainer.scrollTo(offsetWidth(timelineIndex), 0);
+    refreshTimelinesNavButtons(timelineIndex);
+    positionDot.style.left = `${
+      ((blackline.clientWidth - 2 * ((blackline.clientWidth * 3) / 100)) / timelines.length) * (timelineIndex - 1) + (blackline.clientWidth * 3) / 100
+    }px`;
+  }
+});
+nextArea.addEventListener("mouseenter", () => {
+  nextArea.style.opacity = "1";
+});
+nextArea.addEventListener("mouseleave", () => {
+  nextArea.style.opacity = "0";
+});
+nextArea.addEventListener("click", () => {
+  if (timelineIndex < timelines.length) {
+    timelineIndex++;
+    timelineContainer.scrollTo(offsetWidth(timelineIndex), 0);
+    refreshTimelinesNavButtons(timelineIndex);
+    positionDot.style.left = `${
+      ((blackline.clientWidth - 2 * ((blackline.clientWidth * 3) / 100)) / timelines.length) * (timelineIndex - 1) + (blackline.clientWidth * 3) / 100
+    }px`;
+  }
+});
+
+function refreshTimelinesNavButtons(timelineIndex) {
+  previousArea.style.left = `${offsetWidth(timelineIndex)}px`;
+  nextArea.style.right = `-${offsetWidth(timelineIndex)}px`;
+
+  if (timelineIndex == 1) {
+    previousArea.style.display = "none";
+  } else {
+    previousArea.style.display = "flex";
+  }
+
+  if (timelineIndex >= timelines.length) {
+    nextArea.style.display = "none";
+  } else {
+    nextArea.style.display = "flex";
+  }
+}
+function offsetWidth(timelineIndex) {
+  return timelineContainer.clientWidth * (timelineIndex - 1);
+}
+
+// TODO parfois bug de synchro entre timelineIndex et timeLineToDisplay qui empêche le positionnement de la timeline et effectue un déplacement aux flèches standard
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowRight":
+      if (timelineIndex < timelines.length) {
+        timelineIndex++;
+        timelineContainer.scrollTo(offsetWidth(timelineIndex), 0);
+        refreshTimelinesNavButtons(timelineIndex);
+        positionDot.style.left = `${
+          ((blackline.clientWidth - 2 * ((blackline.clientWidth * 3) / 100)) / timelines.length) * (timelineIndex - 1) + (blackline.clientWidth * 3) / 100
+        }px`;
+      } else {
+        e.preventDefault();
+      }
+      break;
+    case "ArrowLeft":
+      if (timelineIndex > 1) {
+        timelineIndex--;
+        timelineContainer.scrollTo(offsetWidth(timelineIndex), 0);
+        refreshTimelinesNavButtons(timelineIndex);
+        positionDot.style.left = `${
+          ((blackline.clientWidth - 2 * ((blackline.clientWidth * 3) / 100)) / timelines.length) * (timelineIndex - 1) + (blackline.clientWidth * 3) / 100
+        }px`;
+      } else {
+        e.preventDefault();
+      }
+      break;
+  }
+});
+
+// Sélection des tags projets
+tagList.forEach((tag) => {
+  tag.addEventListener("click", () => {
+    if (!activeTagList.includes(tag.id)) {
+      activeTagList.push(tag.id);
+      tag.classList.add("project-tag-active");
+    } else {
+      let index = activeTagList.findIndex((tagID) => tagID === tag.id);
+      activeTagList.splice(index, 1);
+      tag.classList.remove("project-tag-active");
+    }
+
+    projectList.forEach((project) => {
+      if (activeTagList.length == 0) {
+        return (project.style.opacity = "1");
+      }
+
+      const hasActiveTag = activeTagList.some((activeTag) => {
+        return project.classList.contains(activeTag);
+      });
+
+      if (hasActiveTag) {
+        return (project.style.opacity = "1");
+      } else {
+        return (project.style.opacity = "0.5");
+      }
+    });
+  });
+});
+
 // Navigation dans la zone strengths
-const titles = document.querySelectorAll(".strengths-headband h4");
-let activeTitle = titles[0];
 activeTitle.classList.add("strengths-headband-active");
 
-PrevText = document.getElementById("strength-1");
 titles.forEach((title, i) => {
   title.addEventListener("click", () => {
     activeTitle.classList.remove("strengths-headband-active");
@@ -120,13 +308,10 @@ titles.forEach((title, i) => {
 });
 
 // Reveal des coordonnées
-const textArea = document.querySelectorAll(".text-area");
-const cordP = document.querySelectorAll(".text-area p");
-
 textArea.forEach((p, i) => {
   p.addEventListener("click", () => {
-    textArea[i].style.color = "#f5f5f5";
-    textArea[i].style.background = "rgb(17, 17, 17)";
+    textArea[i].style.color = "var(--background-color)";
+    textArea[i].style.background = "var(--link-color)";
     if (i == 0) {
       cordP[i].textContent = "06.07.90.04.55";
     }
@@ -137,80 +322,75 @@ textArea.forEach((p, i) => {
 });
 
 // Complétion du formulaire
-const contactCont = document.querySelector("form");
-
-const nameInp = document.getElementById("name");
-const emailInp = document.getElementById("email");
-const telInp = document.getElementById("telephone");
-const radioInp = document.querySelectorAll(".form-part-2-container input");
-const textInp = document.getElementById("description");
-const submInp = document.getElementById("submit");
-let username;
-let email;
-let telephone;
-let choice;
-let message;
-
-nameInp.addEventListener("input", () => {
-  username = nameInp.value;
-  contactCont.style.border = "1px solid white";
-});
-emailInp.addEventListener("input", () => {
-  email = emailInp.value;
-  contactCont.style.border = "1px solid white";
-});
-telInp.addEventListener("input", () => {
-  telephone = telInp.value;
-  contactCont.style.border = "1px solid white";
-});
-radioInp.forEach((radio, x) => {
-  radio.addEventListener("input", () => {
-    if (x == 0) {
-      contactCont.style.border = "1px solid white";
-      choice = "school";
-    } else if (x == 1) {
-      contactCont.style.border = "1px solid white";
-      choice = "recruiter";
-    } else if (x == 2) {
-      contactCont.style.border = "1px solid white";
-      choice = "particular";
-    }
-  });
-});
-textInp.addEventListener("input", () => {
-  contactCont.style.border = "1px solid white";
-  message = textInp.value;
-});
-submInp.addEventListener("click", (e) => {
+contactForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  const username = document.querySelector("#name").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const telephone = document.querySelector("#telephone").value.trim();
+  const choice = document.querySelector('input[name="type"]:checked')?.value.trim();
+  const message = document.querySelector("#description").value.trim();
+
   if (!username || !email || !telephone || !choice || !message) {
-    contactCont.classList.add("notvalid");
-    setTimeout(() => {
-      contactCont.classList.remove("notvalid");
-    }, 2000);
-  } else if (username && email && telephone && choice && message) {
-    contactCont.classList.add("valid");
-    const data = {
-      username,
-      email,
-      telephone,
-      choice,
-      message,
-    };
-    console.log(data);
-
-    nameInp.value = null;
-    emailInp.value = null;
-    telInp.value = null;
-    textInp.value = null;
-    username = null;
-    email = null;
-    telephone = null;
-    choice = null;
-    message = null;
-
-    setTimeout(() => {
-      contactCont.classList.remove("valid");
-    }, 2000);
+    document.querySelector("#name").style.border = "3px solid transparent";
+    document.querySelector("#email").style.border = "3px solid transparent";
+    document.querySelector("#telephone").style.border = "3px solid transparent";
+    document.querySelector(".form-part-2-container").style.border = "3px solid transparent";
+    document.querySelector("#description").style.border = "3px solid transparent";
+    contactForm.style.border = "3px solid red";
+    return (errorMessage.textContent = "Veuillez remplir tous les champs");
   }
+
+  if (!username.match(/[a-zA-Z]/)) {
+    document.querySelector("#name").style.border = "3px solid red";
+    return (errorMessage.textContent = "Votre nom n'est pas valide");
+  }
+
+  if (!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
+    document.querySelector("#email").style.border = "3px solid red";
+    return (errorMessage.textContent = "Votre email n'est pas valide");
+  }
+
+  if (!telephone.match(/^\d[\d.\s-]{1,14}$/)) {
+    document.querySelector("#telephone").style.border = "3px solid red";
+    return (errorMessage.textContent = "Votre numéro de téléphone n'est pas valide");
+  }
+
+  if (choice === undefined) {
+    document.querySelector(".form-part-2-container").style.border = "3px solid red";
+    return (errorMessage.textContent = "Veuillez sélectionner un choix");
+  }
+
+  if (message.length === 0) {
+    document.querySelector("#description").style.border = "3px solid red";
+    return (errorMessage.textContent = "Veuillez ajouter votre message");
+  }
+
+  var formData = new FormData(contactForm);
+  console.log(formData);
+
+  fetch("https://formspree.io/f/xrbadzaz", {
+    method: "POST",
+    body: formData,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return (errorMessage.textContent = "Message envoyé. Merci pour votre intérêt !");
+      }
+      throw new Error("Erreur réseau ou réponse non valide.");
+    })
+    .then(() => {
+      document.querySelector("#name").textContent = "";
+      document.querySelector("#email").textContent = "";
+      document.querySelector("#telephone").textContent = "";
+      // document.querySelector(".form-part-2-container").checkVisibility = "3px solid transparent";
+      document.querySelector("#description").textContent = "";
+    })
+    .catch((error) => {
+      console.error("Erreur:", error);
+      return (errorMessage.textContent = "Une erreur est survenue... veuillez réessayer ulérieurement.");
+    });
 });
